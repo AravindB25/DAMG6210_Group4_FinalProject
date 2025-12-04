17d3565f8f1350a8d92cea233cac7db4acdec43c
pranavpatel0741 <patel.pranavna@northeastern.edu>
2025-12-03 21:47:20 -0500

    update sql files

--- 13_management_reports.sql
+++ 13_management_reports.sql
@@ -1,7 +1,6 @@
 -- ============================================================================
--- Commuter Reservation System (CRS)
+-- Commuter Reservation System (CRS)Group 4
 -- Management Reports and Views
--- Date: November 2025
 -- Note: Run as CRS_ADMIN_USER
 -- ============================================================================
 
